2deffaf89fac3add0978e365067c9bc5d6393cab
Panagiotis Efthymiou <panefthymiou@outlook.com>
2023-12-30 21:04:52 +0200

    Complete Day 4 and 5 challenges.

--- Playground/Day4.cs
+++ Playground/Day4.cs
@@ -2,4 +2,77 @@
 
 internal class Day4
 {
+    internal static int CalculatePoints(string input)
+    {
+        var lines = input.Split('\n', StringSplitOptions.TrimEntries);
+        var totalPoints = 0;
+        foreach (var line in lines)
+        {
+            var parts = line.Split(':', StringSplitOptions.TrimEntries).ToArray();
+            var numbersPart = parts[1].Split("|", StringSplitOptions.TrimEntries).ToArray();
+            var winningNumbers = numbersPart[0]
+                .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
+            var numbers = numbersPart[1]
+                .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
+
+            var points = numbers.Where(number => winningNumbers.Contains(number))
+                .Aggregate(0, (current, _) => current == 0 ? 1 : current * 2);
+
+            totalPoints += points;
+        }
+
+        return totalPoints;
+    }
+
+    internal static int CalculateScratchcards(string input)
+    {
+        var totalScratchcards = 0;
+        var lines = input.Split('\n');
+        List<(int cardNo, int[] winningNumbers, int[] numbers)> cards = [];
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            var parts = line.Split(':', StringSplitOptions.TrimEntries).ToArray();
+            var numbersPart = parts[1].Split("|", StringSplitOptions.TrimEntries).ToArray();
+            var winningNumbers = numbersPart[0]
+                .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var numbers = numbersPart[1]
+                .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            cards.Add((i + 1, winningNumbers, numbers));
+        }
+
+        var index = 0;
+        while (index < cards.Count)
+        {
+            var (cardNo, winningNumbers, numbers) = cards[index];
+            totalScratchcards++;
+            var matches = numbers.Count(number => winningNumbers.Contains(number));
+
+            for (var i = 1; i <= matches && cardNo + i <= input.Length; i++)
+            {
+                var cardIndex = cardNo + i - 1;
+                var card = lines[cardIndex];
+                var parts = card[(card.IndexOf(": ", StringComparison.Ordinal) + 1)..].Split("|", StringSplitOptions.TrimEntries).ToArray();
+                var nextWinningNumbers = parts[0]
+                    .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                var nextNumbers = parts[1]
+                    .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                cards.Add((cardNo + i, nextWinningNumbers, nextNumbers));
+            }
+
+            index++;
+        }
+
+        return totalScratchcards;
+    }
 }
--- Playground/Day5.cs
+++ Playground/Day5.cs
@@ -2,4 +2,71 @@
 
 internal class Day5
 {
+    internal static long CalculateLowestLocationNumber(List<string> input)
+    {
+        var seeds = input[0].Split(" ").Skip(1).Select(long.Parse).ToArray();
+        var mapIds = input.Where(x => x.Contains("map")).ToArray();
+        var maps = mapIds.Select(mapId => ParseMap(input, mapId)).ToList();
+        return seeds.Select(seed => maps.Aggregate(seed, ProcessMap)).Prepend(long.MaxValue).Min();
+    }
+
+    internal static long CalculateLowestLocationNumberFromSeedRange(List<string> input)
+    {
+        var lowestLocationNumber = long.MaxValue;
+        var ranges = ParseSeedRanges(input[0].Split(" ").Skip(1).Select(long.Parse).ToArray());
+        var mapIds = input.Where(x => x.Contains("map")).ToArray();
+        var maps = mapIds.Select(mapId => ParseMap(input, mapId)).ToList();
+
+        foreach (var range in ranges)
+        {
+            for (var i = 0; i < range.Item2; i++)
+            {
+                var currentSeed = range.Item1 + i;
+                currentSeed = maps.Aggregate(currentSeed, ProcessMap);
+                lowestLocationNumber = Math.Min(lowestLocationNumber, currentSeed);
+            }
+        }
+
+        return lowestLocationNumber;
+    }
+
+    private static List<(long, long)> ParseSeedRanges(IReadOnlyList<long> input)
+    {
+        List<(long, long)> ranges = [];
+
+        for (var i = 0; i < input.Count; i += 2)
+        {
+            if (i + 1 < input.Count)
+            {
+                ranges.Add((input[i], input[i + 1]));
+            }
+        }
+        return ranges;
+    }
+
+    private static List<(long, long, long)> ParseMap(List<string> input, string mapId)
+    {
+        List<(long, long, long)> map = [];
+
+        var start = input.IndexOf(mapId) + 1;
+        var tempList = input.GetRange(start, input.Count - start);
+        var end = tempList.FindIndex(x => x.Contains("map"));
+        var count = end == -1 ? tempList.Count : end - 1;
+        var lines = tempList.GetRange(0, count);
+
+        map.AddRange(lines.Select(line => line.Split(' '))
+            .Select(parts => (long.Parse(parts[0]), long.Parse(parts[1]), long.Parse(parts[2]))));
+
+        return map;
+    }
+
+    private static long ProcessMap(long seed, List<(long, long, long)> map)
+    {
+        foreach (var entry in map.Where(entry => seed >= entry.Item2 && seed < entry.Item2 + entry.Item3))
+        {
+            return entry.Item1 + (seed - entry.Item2);
+        }
+
+        return seed;
+    }
 }
